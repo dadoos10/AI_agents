f92442286966cb16ceb1fbbb8ffdf059ff803e37
dadoos10 <79057071+dadoos10@users.noreply.github.com>
2025-04-22 12:54:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from langchain_core.output_parsers import PydanticOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.agents import create_tool_calling_agent, AgentExecutor
 from langchain_openai import ChatOpenAI
-import os
-from dotenv import load_dotenv
 from tools import search_tool, wiki_tool, save_tool
 
 load_dotenv()
